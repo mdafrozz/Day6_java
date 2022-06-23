99af4d307c09d7d0f9192086b46d81f10247a665
mdafrozz <mdafroz.gm@gmail.com>
2022-06-23 09:55:24 +0530

    refactor: usecase

---
+++ StopWatch/StopWatch.java
@@ -0,0 +1,48 @@
+package StopWatch;
+
+public class StopWatch {
+	public long startTimer=0;
+	public long stopTimer=0;
+	public long elapsed;
+	
+	public void start()
+	{
+		startTimer = System.currentTimeMillis();
+		System.out.println("Start Time is: " + startTimer);
+	}
+	
+	public void stop()
+	{
+		stopTimer = System.currentTimeMillis();
+		System.out.println("Stop Time is: " + stopTimer);
+	}
+	
+	public long getElapsedTime()
+	{
+		elapsed = stopTimer - startTimer;
+		return elapsed;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		
+		StopWatch sw = new StopWatch();
+		helper hp = new helper();
+		System.out.println("Press '1' to Start Time: ");
+		hp.inputInteger();
+		sw.start();
+		
+		System.out.println();
+		System.out.println("Press '2' to Stop Time: ");
+		hp.inputInteger();
+		sw.stop();
+		
+		long l=sw.getElapsedTime();
+		System.out.println();
+		System.out.println("Total Time Elapsed(in millisec) is:" + l);
+		System.out.println();
+		System.out.println("Converting millisec to seconds: "+ (l/1000) + " sec");
+
+	}
+}
